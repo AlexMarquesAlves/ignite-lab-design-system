236a75d0907f1d4039d81b4870d24ee858f2cac1
¡Duque <atanaelleonardo@gmail.com>
2022-10-11 19:53:38 -0300

    🆕 Inter Font imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import "./styles/global.css";
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+
+      <button></button>
+    </>
+  );
 }
