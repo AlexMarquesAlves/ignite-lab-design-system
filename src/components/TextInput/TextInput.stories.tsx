28857d1100f669f8e897b740b49954f92a119618
¡Duque <atanaelleonardo@gmail.com>
2022-10-12 03:00:29 -0300

    🆕 TextInput stories added

---
+++ src/components/TextInput/TextInput.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { TextInput, TextInputProps } from ".";
+
+export default {
+  title: "components/TextInput",
+  component: TextInput,
+  args: {},
+  argTypes: {},
+} as Meta<TextInputProps>;
+
+export const Default: StoryObj<TextInputProps> = {};
