8e267133d8fcd8a25502da32b2d22422cf0a88fe
¡Duque <atanaelleonardo@gmail.com>
2022-10-11 21:11:07 -0300

    🆕 Text stories added

---
+++ src/components/Text/Text.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "components/Text",
+  component: Text,
+} as Meta;
+
+export const Default: StoryObj = {};
+
+export const Small: StoryObj = {};
+export const Large: StoryObj = {};
