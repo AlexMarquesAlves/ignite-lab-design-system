5a19f6fc808a280345b9a891ff01c92843edf27f
¡Duque <atanaelleonardo@gmail.com>
2022-10-12 03:07:27 -0300

    🆕 Styles for TextInput

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -3,5 +3,12 @@ import { clsx } from "clsx";
 export type TextInputProps = {};
 
 export const TextInput = ({}: TextInputProps) => {
-  return <input className={clsx(``, {})} />;
+  return (
+    <input
+      className={clsx(
+        `py-4 px-3 rounded bg-gray-800 text-gray-100 text-xs placeholder:text-gray-400 w-full`,
+        {},
+      )}
+    />
+  );
 };
